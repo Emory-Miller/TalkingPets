ed7cc4de59612249ecb8fead8df670906b680067
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-11-19 16:53:51 -0500

    Checkpointing work for this lab

---
+++ src/main/java/io/zipcoder/polymorphism/Console.java
@@ -0,0 +1,41 @@
+package io.zipcoder.polymorphism;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+
+public class Console {
+
+    private final Scanner input;
+    private final PrintStream output;
+
+    public Console (InputStream in, PrintStream out){
+        this.input = new Scanner(in);
+        this.output = out;
+    }
+
+    public void print(String val, Object... args){
+        output.format(val, args);
+    }
+
+    public void println(String val, Object... vals){
+        print(val + "\n", vals);
+    }
+
+    public String getStringInput(String prompt, Object... args){
+        println(prompt, args);
+        return input.nextLine();
+    }
+
+    public int getIntInput(String prompt, Object...args){
+        String stringInput = getStringInput(prompt, args);
+        try{
+            return Integer.parseInt(stringInput);
+        } catch (NumberFormatException nfe){
+            println("[ %s ] is an invalid user input!", stringInput);
+            println("Try inputting an integer value!");
+            return getIntInput(prompt, args);
+        }
+    }
+}
--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,4 +1,42 @@
 package io.zipcoder.polymorphism;
 
+import java.util.ArrayList;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
 public class MainApplication {
+    static Console console = new Console(in, out);
+
+    ArrayList<Pet> petsOwnedList = new ArrayList<>();
+
+    static ArrayList<String> stringList = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        int numPets = console.getIntInput("How many pets do you have?");
+
+        for (int i = 0; i < numPets; i++) {
+            String type = console.getStringInput("What kind of pet is it?");
+            String name = console.getStringInput("Whats the name of your pet?");
+            petFromInput(type, name);
+
+        }
+
+        console.println(stringList.toString());
+
+//        for (Pet pet : petsOwnedList){
+//            console.println(pet.speak());
+//        }
+    }
+
+    public static Pet petFromInput(String type, String name) {
+        if (type.equalsIgnoreCase("dog")) {
+            new Dog(name);
+        } else if (type.equalsIgnoreCase("cat")) {
+            new Cat(name);
+        } else if (type.equalsIgnoreCase("bird")) {
+            new Bird(name);
+        } return new Pet(name);
+    }
 }
