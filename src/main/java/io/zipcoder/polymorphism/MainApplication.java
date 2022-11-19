b7721e019f43a5a5a9ad60ec32209f0061c826ee
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-11-19 17:12:50 -0500

    checkpointing my work, need to do tests

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -8,7 +8,7 @@ import static java.lang.System.out;
 public class MainApplication {
     static Console console = new Console(in, out);
 
-    ArrayList<Pet> petsOwnedList = new ArrayList<>();
+    static ArrayList<Pet> petsOwnedList = new ArrayList<>();
 
     static ArrayList<String> stringList = new ArrayList<>();
 
@@ -19,24 +19,23 @@ public class MainApplication {
         for (int i = 0; i < numPets; i++) {
             String type = console.getStringInput("What kind of pet is it?");
             String name = console.getStringInput("Whats the name of your pet?");
-            petFromInput(type, name);
-
+            petsOwnedList.add(petFromInput(type, name));
         }
 
-        console.println(stringList.toString());
+        for (Pet pets : petsOwnedList){
+            console.println(pets.speak());
+        }
 
-//        for (Pet pet : petsOwnedList){
-//            console.println(pet.speak());
-//        }
     }
 
     public static Pet petFromInput(String type, String name) {
         if (type.equalsIgnoreCase("dog")) {
-            new Dog(name);
+            return new Dog(name);
         } else if (type.equalsIgnoreCase("cat")) {
-            new Cat(name);
+            return new Cat(name);
         } else if (type.equalsIgnoreCase("bird")) {
-            new Bird(name);
-        } return new Pet(name);
+            return new Bird(name);
+        } return null;
     }
+
 }
